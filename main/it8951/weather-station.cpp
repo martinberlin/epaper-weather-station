ed201447f318b4363593f8cd1133ca4c4654966c
martinberlin <martin@luckycloud.de>
2022-08-21 08:21:22 +0200

    Update Lovyan and try to setVCOM()

--- main/it8951/weather-station.cpp
+++ main/it8951/weather-station.cpp
@@ -36,7 +36,9 @@
 #define TPS_POWER_MODE GPIO_NUM_5
 
 // Clock will refresh every:
-#define DEEP_SLEEP_SECONDS 60
+#define DEEP_SLEEP_SECONDS 120
+// Avoids printing text always in same place so we don't leave marks in the epaper (Although parallel get well with that)
+#define X_RANDOM_MODE true
 uint64_t USEC = 1000000;
 // Weekdays and months translatables
 char weekday_t[][12] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
@@ -142,6 +144,14 @@ public:
 };
 LGFX display;
 
+uint16_t generateRandom(uint16_t max) {
+    if (max>0) {
+        srand(esp_timer_get_time());
+        return rand() % max;
+    }
+    return 0;
+}
+
 void delay_ms(uint32_t period_ms) {
     ets_delay_us(period_ms * 1000);
 }
@@ -189,8 +199,12 @@ static bool obtain_time(void)
 }
 
 void deep_sleep() {
-    // Turn off the 3.7 to 5V step-up
-    gpio_set_level(GPIO_ENABLE_5V, 0);
+    // Turn off the 3.7 to 5V step-up and put all IO pins in INPUT mode
+    uint8_t EP_CONTROL[] = {CONFIG_EINK_SPI_CLK, CONFIG_EINK_SPI_MOSI, CONFIG_EINK_SPI_MISO, CONFIG_EINK_SPI_CS, GPIO_ENABLE_5V};
+    for (int io = 0; io < 5; io++) {
+        gpio_set_level((gpio_num_t) EP_CONTROL[io], 0);
+        gpio_set_direction((gpio_num_t) EP_CONTROL[io], GPIO_MODE_INPUT);
+    }
     ESP_LOGI(pcTaskGetName(0), "DEEP_SLEEP_SECONDS: %d seconds to wake-up", DEEP_SLEEP_SECONDS);
     esp_sleep_enable_timer_wakeup(DEEP_SLEEP_SECONDS * USEC);
     esp_deep_sleep_start();
@@ -245,7 +259,9 @@ void setClock(void *pvParameters)
     display.println("Initial date time\nis saved on RTC\n");
 
     display.printf("%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min);
-    vTaskDelay(pdMS_TO_TICKS(150));
+
+    // Wait some time to see if disconnecting all changes background color
+    delay_ms(50); 
     // goto deep sleep
     deep_sleep();
 }
@@ -254,7 +270,7 @@ void getClock(void *pvParameters)
 {
     // Initialise the xLastWakeTime variable with the current time.
     // TickType_t xLastWakeTime = xTaskGetTickCount();
-    // printf("getClock()\n");
+
     // Get RTC date and time
     float temp;
     struct tm rtcinfo;
@@ -279,7 +295,11 @@ void getClock(void *pvParameters)
       display.fillScreen(display.color888(0,0,0));
     }
     // Print day
-    display.setCursor(100, y_start-20);
+    uint16_t x_cursor = 100;
+    if (X_RANDOM_MODE) {
+        x_cursor = 100 + generateRandom(400);
+    }
+    display.setCursor(x_cursor, y_start-20);
     display.setTextColor(display.color888(200,200,200));
     display.setFont(&DejaVuSans_Bold60pt7b);
     display.printf("%s", weekday_t[rtcinfo.tm_wday]);
@@ -291,7 +311,11 @@ void getClock(void *pvParameters)
     if (DARK_MODE) {
         color = display.color888(0,0,0);
     }
-    display.setCursor(100, y_start);
+    x_cursor = 100;
+    if (X_RANDOM_MODE) {
+        x_cursor = 100 + generateRandom(350);
+    }
+    display.setCursor(x_cursor, y_start);
     display.fillRect(100, y_start+10, EPD_WIDTH-100 , 200, color);
 
     // Print clock HH:MM (Seconds excluded: rtcinfo.tm_sec)
@@ -305,7 +329,11 @@ void getClock(void *pvParameters)
     // Print date YYYY-MM-DD update format as you want
     display.setFont(&Ubuntu_M48pt8b);
     y_start+=200;
-    display.setCursor(100,y_start);
+    x_cursor = 100;
+    if (X_RANDOM_MODE) {
+        x_cursor = 100 + generateRandom(400);
+    }
+    display.setCursor(x_cursor, y_start);
     display.setTextColor(display.color888(70,70,70));
     display.setTextSize(1);
     // N month, year
@@ -315,13 +343,17 @@ void getClock(void *pvParameters)
 
     // Print temperature
     y_start+=180;
-    display.fillRect(100, y_start+20, EPD_WIDTH/2 , 200, color);
+    x_cursor = 100;
+    if (X_RANDOM_MODE) {
+        x_cursor = 100 + generateRandom(300);
+    }
+    display.fillRect(x_cursor, y_start+20, EPD_WIDTH/2 , 200, color);
     display.setTextColor(display.color888(170,170,170));
-    display.setCursor(100,y_start);
+    display.setCursor(x_cursor, y_start);
     display.printf("%.2f C", temp);
    
     display.setFont(&Ubuntu_M24pt8b);
-    display.setCursor(EPD_WIDTH-400,y_start);
+    display.setCursor(EPD_WIDTH-300,y_start);
     uint16_t vcom = display.getVCOM();
     display.printf("vcom:%d", vcom);
     delay_ms(200);
@@ -399,7 +431,7 @@ void app_main()
     // Turn on the 3.7 to 5V step-up
     gpio_set_level(GPIO_ENABLE_5V, 1);
     // Wait until board is fully powered
-    delay_ms(50);
+    delay_ms(80);
 
     // Initialize NVS
     esp_err_t err = nvs_flash_init();
@@ -424,16 +456,19 @@ void app_main()
     nvs_set_i16(my_handle, "boots", nvs_boots);
 
     display.init();
-    //display.setVCOM(1178); // -1.78 V
-    
+
+    // Commenting this VCOM is set as default to 2600 (-2.6) which is too high for most epaper displays
+    // Leaving that value you might see gray background since it's the top reference voltage
     if (nvs_boots%2 == 0) {
-      display.clearDisplay();
-      vTaskDelay(pdMS_TO_TICKS(100));
+        printf("setVCOM()\n)");
+        display.setVCOM(1900);          // 1780 -1.78 V
+
+        //vTaskDelay(pdMS_TO_TICKS(4500)); // 4500 ms is the aprox. wait for this board VCOM
+        display.clearDisplay();
     }
 	// epd_fast:    LovyanGFX uses a 4×4 16pixel tile pattern to display a pseudo 17level grayscale.
 	// epd_quality: Uses 16 levels of grayscale
 	display.setEpdMode(epd_mode_t::epd_fast);
-
 
     // Initialize RTC
     if (ds3231_init_desc(&dev, I2C_NUM_0, (gpio_num_t) CONFIG_SDA_GPIO, (gpio_num_t) CONFIG_SCL_GPIO) != ESP_OK) {
