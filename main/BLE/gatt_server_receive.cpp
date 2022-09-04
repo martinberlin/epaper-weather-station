ab708a5ef861b49cfe412ae33e711f143d9283f5
martinberlin <martin@luckycloud.de>
2022-09-04 18:38:10 +0200

    Cleanup. Wrap decodeJpeg in between startWrite/ endWrite

--- main/BLE/gatt_server_receive.cpp
+++ main/BLE/gatt_server_receive.cpp
@@ -64,14 +64,13 @@ uint32_t received_length = 0;
 // Draws a progress bar when downloading (Just a demo: is faster without it)
 // And also writes in the same framebuffer as the image
 #define DOWNLOAD_PROGRESS_BAR true
-uint8_t progressBarHeight = 4;
+uint8_t progressBarHeight = 10;
 
 
-#define GATTS_TAG "GATTS_JPG"
+#define GATTS_TAG "BLE"
 
 ///Declare the static function
 static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
-
 
 /*                           LSB <----------------------> MSB */
 // 0000180d-0000-1000-8000-00805f9b34fb
@@ -81,7 +80,7 @@ static uint16_t BLE_SERVICE_UUID = 0x180d; // Short Service UUID
 static uint16_t GATTS_CHAR_UUID_TEST_A = 0x180d;
 #define GATTS_NUM_HANDLE_TEST_A     4
 
-#define TEST_DEVICE_NAME            "EPD_BLE_JPEG"
+#define TEST_DEVICE_NAME            "BLE_JPEG_097"
 #define TEST_MANUFACTURER_DATA_LEN  17
 
 #define GATTS_DEMO_CHAR_VAL_LEN_MAX 16
@@ -274,7 +273,7 @@ LGFX display;
 void progressBar(long processed, long total)
 {
   int percentage = round(processed * EPD_WIDTH / total);
-  display.fillRect(0, 0, percentage, progressBarHeight);
+  display.drawRect(0, 0, percentage, progressBarHeight);
 }
 
 //====================================================================================
@@ -538,8 +537,9 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
                 ESP_LOGI(GATTS_TAG, "0x09 EOF received");
                 // Decode & render
                 time_receive = (esp_timer_get_time()-start_time)/1000;
+                display.startWrite();
                 decodeJpeg(source_buf, 0, 0);
-
+                display.endWrite();
                 ESP_LOGI("ble-rec", "%d ms - download", time_receive);
                 ESP_LOGI("render", "%d ms - copying pix", time_render);
 
@@ -561,42 +561,6 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
             } */
             if (received_events%10 == 0) {
                 progressBar(img_buf_pos, received_length);
-            }
-
-            if (gl_profile_tab[PROFILE_A_APP_ID].descr_handle == param->write.handle && param->write.len == 2){
-                uint16_t descr_value = param->write.value[1]<<8 | param->write.value[0];
-                if (descr_value == 0x0001){
-                    if (a_property & ESP_GATT_CHAR_PROP_BIT_NOTIFY){
-                        ESP_LOGI(GATTS_TAG, "notify enable");
-                        uint8_t notify_data[15];
-                        for (int i = 0; i < sizeof(notify_data); ++i)
-                        {
-                            notify_data[i] = i%0xff;
-                        }
-                        //the size of notify_data[] need less than MTU size
-                        esp_ble_gatts_send_indicate(gatts_if, param->write.conn_id, gl_profile_tab[PROFILE_A_APP_ID].char_handle,
-                                                sizeof(notify_data), notify_data, false);
-                    }
-                }else if (descr_value == 0x0002){
-                    if (a_property & ESP_GATT_CHAR_PROP_BIT_INDICATE){
-                        ESP_LOGI(GATTS_TAG, "indicate enable");
-                        uint8_t indicate_data[15];
-                        for (int i = 0; i < sizeof(indicate_data); ++i)
-                        {
-                            indicate_data[i] = i%0xff;
-                        }
-                        //the size of indicate_data[] need less than MTU size
-                        esp_ble_gatts_send_indicate(gatts_if, param->write.conn_id, gl_profile_tab[PROFILE_A_APP_ID].char_handle,
-                                                sizeof(indicate_data), indicate_data, true);
-                    }
-                }
-                else if (descr_value == 0x0000){
-                    ESP_LOGI(GATTS_TAG, "notify/indicate disable ");
-                }else{
-                    ESP_LOGE(GATTS_TAG, "unknown descr value");
-                    esp_log_buffer_hex(GATTS_TAG, param->write.value, param->write.len);
-                }
-
             }
         }
         example_write_event_env(gatts_if, &a_prepare_write_env, param);
@@ -621,7 +585,8 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
         gl_profile_tab[PROFILE_A_APP_ID].char_uuid.uuid.uuid16 = GATTS_CHAR_UUID_TEST_A; // GATTS_CHAR_UUID_TEST_A
 
         esp_ble_gatts_start_service(gl_profile_tab[PROFILE_A_APP_ID].service_handle);
-        a_property = ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_NOTIFY;
+        // Added ESP_GATT_CHAR_PROP_BIT_WRITE_NR
+        a_property = ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_WRITE_NR | ESP_GATT_CHAR_PROP_BIT_NOTIFY;
         esp_err_t add_char_ret = esp_ble_gatts_add_char(gl_profile_tab[PROFILE_A_APP_ID].service_handle, 
                                                         &gl_profile_tab[PROFILE_A_APP_ID].char_uuid,
                                                         ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
