7f8772a473c40da1e5c1f720549b1925c79531dc
martinberlin <martin@luckycloud.de>
2023-02-06 10:42:43 +0100

    Add also german translation

---
+++ main/it8951/translations/deutsch.h
@@ -0,0 +1,8 @@
+// German translation
+char weekday_t[][12] = { "Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag" };
+char month_t[][12] = { "Januar", "Februar", "März", "April", "Mai", "Juni", 
+                        "Juli", "August", "September", "Oktober", "November", "Dezember"};
+
+char temperature_suffix[] = "C";
+char co2_suffix[]         = "CO2";
+char humidity_suffix[]    = "% H";
